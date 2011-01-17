b9891a9aeb9f4e1d58633fe05cd65143b675e340
Hoat Le Van <hoatlevan@gmail.com>
2011-01-17 19:06:56 +0700

    Add default maven project created from archetype plugin

---
+++ exo.samples.scheduler/src/main/java/org/exoplatform/samples/scheduler/App.java
@@ -0,0 +1,13 @@
+package org.exoplatform.samples.scheduler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
